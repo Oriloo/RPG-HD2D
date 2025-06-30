17218553358827943491d5d24fbfe4e79c107a14
Pol-Mattis <88mangagemu@gmail.com>
2025-06-30 09:51:34 +0200

    fix: Thanos snaps his fingers

--- Source/RPG_HD2D/RPG_HD2D.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RPG_HD2D.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPG_HD2D, "RPG_HD2D" );
